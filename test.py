8c80358985944a7ca8c83d29a84d8b238c2f3e41
newhiwoong <newhiwoong@gmail.com>
2017-05-18 16:55:11 +0900

    graph-test

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import plot, show, title, xlabel, ylabel
 f = open("lyrics.txt")
 lyrics = []
 word = 0
-
+y=[1.3424,2.341,3.342,0,4.2134,1.2341,1.422,2.3432]
 while True:
 	line = f.readline()
 	if not line:
@@ -23,4 +26,8 @@ print (len(rr), len(rrr), len(rrrr))
 a =[1,2,3,4,5,6]
 print(sum(a))
 
-
+title('Score by line')
+xlabel('line')
+ylabel('score')
+plot(y,marker="o")
+show()
