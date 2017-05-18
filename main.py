7926da8a976c4dafb8f226aad87008ed418911b8
newhiwoong <newhiwoong@gmail.com>
2017-05-18 13:27:11 +0900

    plus-time

--- main.py
+++ main.py
@@ -3,23 +3,35 @@ import time
 point = 0
 f = open("lyrics.txt")
 lyrics = []
+timez = []
+
 while True:
 	line = f.readline()
 	if not line:
 		break
-	lyrics.append(line[:-2])
+	lyrics.append(line[:-1])
 f.close()
 
 input("Chat a lyrics:")
 start = time.time()
 
 for ly in lyrics:
+	answer = 0
+	stL = time.time()
 	print(ly)
 	lc = input("")
 	if lc == ly:
-		point += 1
+		answer = 1
+	point += answer
+	edL = time.time()
+	timez.append(answer*len(lyrics)/(edL-stL))
+
 
 end = time.time()
 et = end - start
 et = format(et, ".2f")
 print("Chat : ", et,"second\n",point,"point")
+for t in timez:
+	print(t)
+print("정확도 : ",point/len(lyrics))
+print(sum(timez))
--- test.py
+++ test.py
@@ -6,8 +6,8 @@ while True:
 	line = f.readline()
 	if not line:
 		break
-	lyrics.append(line[:-2])
-	word += len(line[:-2])
+	lyrics.append(line[:-1])
+	word += len(line[:-1])
 f.close()
 
 for i in lyrics:
@@ -19,3 +19,8 @@ rr = "안녕하세요"
 rrr = "hi"
 rrrr = "안i"
 print (len(rr), len(rrr), len(rrrr))
+
+a =[1,2,3,4,5,6]
+print(sum(a))
+
+
